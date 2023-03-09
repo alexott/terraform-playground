ab432ed5c86e32974d51867f32abe58c48cef6e1
Alex Ott <alexey.ott@databricks.com>
2023-03-09 21:21:36 +0100

    add example for init script on ABFSS

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Different snippets of Terraform code. Primarily around Databricks
 * [dlt-on-abfss](dlt-on-abfss) shows how to configure Delta Live Table (DLT) pipeline to store data in Azure Storage (ABFSS protocol).
 * [dlt-t-shirt-sizing](dlt-t-shirt-sizing) shows how to create a reusable Terraform module for DLT pipeline that is configured by T-shirt size-style specification instead of using hardcoded min/max number of nodes.
 * [akv-to-db-secrets](akv-to-db-secrets) shows how to replicate secrets from Azure Key Vault into the Databricks Secrets scope (for example, to access them in another cloud)
+* [cluster-init-script-on-abfss](cluster-init-script-on-abfss) shows how to configure cluster to use init script from the Azure Storage Account (using `abfss` protocol)
---
+++ cluster-init-script-on-abfss/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
+
+provider "databricks" {
+  # Configuration options
+}
+
+# FILL THIS!
+locals {
+  azure_tenant_id                  = ""
+  azure_client_id                  = ""
+  azure_client_secret_secret_scope = ""
+  azure_client_secret_secret_key   = ""
+  abfss_init_script_path           = "abfss://<container>@<storage_account>.dfs.core.windows.net/init_script.sh"
+}
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
+
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+}
+
+resource "databricks_cluster" "shared_autoscaling" {
+  cluster_name            = "Test ABFSS init script"
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = data.databricks_node_type.smallest.id
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 1
+    max_workers = 10
+  }
+
+  spark_conf = {
+    "spark.hadoop.fs.azure.account.auth.type" : "OAuth",
+    "spark.hadoop.fs.azure.account.oauth.provider.type" : "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+    "spark.hadoop.fs.azure.account.oauth2.client.endpoint" : "https://login.microsoftonline.com/${local.azure_tenant_id}/oauth2/token",
+    "spark.hadoop.fs.azure.account.oauth2.client.id" : local.azure_client_id,
+    "spark.hadoop.fs.azure.account.oauth2.client.secret" : "{{secrets/${local.azure_client_secret_secret_scope}/${local.azure_client_secret_secret_key}}}"
+  }
+
+  init_scripts {
+    abfss {
+      destination = local.abfss_init_script_path
+    }
+  }
+}
